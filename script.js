eab4f05dd1ec91c0768f99da7dfcba21ca62c5a8
Lucas Brandão <lbrandaodev@gmail.com>
2023-04-27 15:11:41 -0300

    refactory script button

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-let play = document.querySelector('.play')
-let pause = document.querySelector('.pause')
-let stop = document.querySelector('.stop')
-let soundON = document.querySelector('.sound-on')
-let soundOf = document.querySelector('.sound-of')
+let buttonplay = document.querySelector('.play')
+let buttonpause = document.querySelector('.pause')
+let buttonstop = document.querySelector('.stop')
+let buttonsoundON = document.querySelector('.sound-on')
+let buttonsoundOf = document.querySelector('.sound-of')
 
 
 
-play.addEventListener('click', function() {
-  play.classList.add('hide')
+buttonplay.addEventListener('click', function() {
+  buttonplay.classList.add('hide')
   document.querySelector('.pause').classList.remove('hide')
   document.querySelector('.stop').classList.remove('hide')
   document.querySelector('.set').classList.add('hide')
   
 })
 
-pause.addEventListener('click', function() {
-  pause.classList.add('hide')
+buttonpause.addEventListener('click', function() {
+  buttonpause.classList.add('hide')
   document.querySelector('.play').classList.remove('hide')
   document.querySelector('.set').classList.add('hide')
   
   
 })
 
-stop.addEventListener('click', function() {
-  stop.classList.add('hide')
+buttonstop.addEventListener('click', function() {
+  buttonstop.classList.add('hide')
   document.querySelector('.set').classList.remove('hide')
   document.querySelector('.play').classList.remove('hide')
   document.querySelector('.pause').classList.add('hide')
   
 })
 
-soundON.addEventListener('click', function() {
-  soundON.classList.add('hide')
+buttonsoundON.addEventListener('click', function() {
+  buttonsoundON.classList.add('hide')
   document.querySelector('.sound-of').classList.remove('hide')
 
 })
 
-soundOf.addEventListener('click', function() {
-  soundOf.classList.add('hide')
+buttonsoundOf.addEventListener('click', function() {
+  buttonsoundOf.classList.add('hide')
   document.querySelector('.sound-on').classList.remove('hide')
 })
 
