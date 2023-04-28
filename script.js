9065a361097053c1f99ff00835973b41749dfe66
Lucas Brandão <lbrandaodev@gmail.com>
2023-04-28 11:38:46 -0300

    commit refactory controls e logic timer

--- script.js
+++ script.js
@@ -4,14 +4,57 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundON = document.querySelector('.sound-on')
 const buttonSoundOf = document.querySelector('.sound-of')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
 
+function resetControls () {
+  buttonStop.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+}
 
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
+  secondsDisplay.textContent = String(seconds).padStart(2, '0')
+}
+
+function countDown () {
+  setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+  
+    updateTimerDisplay(minutes, 0)
+    
+    if(minutes <= 0 ){
+
+      resetControls()
+
+      return
+    }
+    
+    if(seconds <= 0) {
+      seconds = 2
+      --minutes
+
+    }
+    
+    updateTimerDisplay(minutes, String(seconds -1))
+    
+    
+    countDown()
+    }, 1000)
+}
 
 buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonStop.classList.remove('hide')
   buttonSet.classList.add('hide')
+
+
+  countDown()
   
 })
 
@@ -19,28 +62,32 @@ buttonPause.addEventListener('click', function() {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
   buttonSet.classList.add('hide')
-  
-  
+    
 })
 
 buttonStop.addEventListener('click', function() {
-  buttonStop.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
+
+  resetControls()
   
 })
 
 buttonSoundON.addEventListener('click', function() {
   buttonSoundON.classList.add('hide')
   buttonSoundOf.classList.remove('hide')
-
+  
 })
 
 buttonSoundOf.addEventListener('click', function() {
   buttonSoundOf.classList.add('hide')
   buttonSoundON.classList.remove('hide')
+
 })
 
+
+buttonSet.addEventListener('click', function() {
+  minutes = prompt('Quantos minutos?')
+  updateTimerDisplay(minutes, 0)
+  
+})
 
 
