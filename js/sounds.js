1a3acef2b11ccd585e0faa49e46529e9d5a1830f
Lucas Brandão <lucasbranndao@gmail.com>
2023-05-03 13:21:38 -0300

    commit link audio and favico

--- js/sounds.js
+++ js/sounds.js
@@ -1,7 +1,9 @@
 export default function() {
-const buttonPressAudio = new Audio("https://github.com/lucasbranndao/FocusTimer/blob/main/sound/Buttonplay.mp3")
+const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
 const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
-const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/bg-audio.cp3?raw=true")
+const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
+
+bgAudio.loop
 
 function pressButton() {
   buttonPressAudio.play()
@@ -9,8 +11,8 @@ function pressButton() {
 
 function timerEnd() {
   kitchenTimer.play()
-}
 
+}
 
 return {
   pressButton,
@@ -19,5 +21,3 @@ return {
 }
 
 }
-
-// https://www.youtube.com/results?search_query=button+press+audio
