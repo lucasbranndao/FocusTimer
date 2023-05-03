78d1e37baa42d7382b66ee963011f25c347032f4
Lucas Brandão <lucasbranndao@gmail.com>
2023-05-03 12:52:11 -0300

    delete paste sound

--- js/sounds.js
+++ js/sounds.js
@@ -1,5 +1,5 @@
 export default function() {
-const buttonPressAudio = new Audio("audio")
+const buttonPressAudio = new Audio("https://github.com/lucasbranndao/FocusTimer/blob/main/sound/Buttonplay.mp3")
 const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
 const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/bg-audio.cp3?raw=true")
 
